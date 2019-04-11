f9ba190dac5a401640246ec2729f10351867c7a3
rohmaddj <rohmaddj@gmail.com>
2019-04-11 08:12:31 +0700

    learn static route

--- src/golang-web/main.go
+++ src/golang-web/main.go
@@ -3,25 +3,12 @@ package main
 import "fmt"
 import "net/http"
 
-func handlerIndex(w http.ResponseWriter, r *http.Request)  {
-	message := "Welcome"
-	w.Write([]byte(message))
-}
-
-func handlerHello(w http.ResponseWriter, r *http.Request) {
-	message := "Hello World!"
-	w.Write([]byte(message))
-}
 
 func main() {
-	http.HandleFunc("/", handlerIndex)
-	http.HandleFunc("/index", handlerIndex)
-	http.HandleFunc("/hello", handlerHello)
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("assets"))))
 
-	address := "localhost:9000"
-	fmt.Printf("server running at %s\n", address)
-	err := http.ListenAndServe(address, nil)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	fmt.Println("server running at localhost:9000")
+	http.ListenAndServe(":9000", nil)
 }
