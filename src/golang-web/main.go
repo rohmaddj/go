cb4c99017f0705fa7c16bab6ef746c73c972258f
rohmaddj <rohmaddj@gmail.com>
2019-04-11 08:51:32 +0700

    partial template with ParseGlob function

--- src/golang-web/main.go
+++ src/golang-web/main.go
@@ -1,35 +1,36 @@
 package main
 
-import "fmt"
-import "net/http"
-import "html/template"
-import "path"
+import (
+	"net/http"
+	"html/template"
+	"fmt"
+)
+
+type M map[string]interface{}
 
 func main() {
-	http.Handle("/static/",
-		http.StripPrefix("/static/",
-			http.FileServer(http.Dir("assets"))))
-			
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		filepath := path.Join("views", "index.html")
-		tmpl, err := template.ParseFiles(filepath)
-		
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	tmpl, err := template.ParseGlob("views/*")
+	if err != nil {
+		panic(err.Error())
+		return
+	}
 
-		data := map[string]interface{}{
-			"title": "Learning Golang Web",
-			"name": "Batman",
-		}
-
-		err = tmpl.Execute(w, data)
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		data := M{"name": "Batman"}
+		err = tmpl.ExecuteTemplate(w, "index", data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
-	fmt.Println("Server running at localhost:9000")
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		data := M{"name": "Baman"}
+		err = tmpl.ExecuteTemplate(w, "about", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println("server running at localhost:9000")
 	http.ListenAndServe(":9000", nil)
 }
