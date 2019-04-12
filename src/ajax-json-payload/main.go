d4ffc5285e1ee94ae26a80d6fdefebd3c08b4ac2
rohmaddj <rohmaddj@gmail.com>
2019-04-12 13:35:36 +0700

    ajax json request

---
+++ src/ajax-json-payload/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/save", handleSave)
+
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("assets"))))
+
+	fmt.Println("server running at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handleSave(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		decoder := json.NewDecoder(r.Body)
+		payload := struct {
+			Name   string `json:"name"`
+			Age    int    `json:"age"`
+			Gender string `json:"gender"`
+		}{}
+		if err := decoder.Decode(&payload); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		message := fmt.Sprintf(
+			"hello, my name is %s. I am %d year old %s",
+			payload.Name,
+			payload.Age,
+			payload.Gender,
+		)
+		w.Write([]byte(message))
+		return
+	}
+	http.Error(w, "Only accept POST request", http.StatusBadRequest)
+}
